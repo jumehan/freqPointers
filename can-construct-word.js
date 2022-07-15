92bd078ce94cf8c309f782092b1e63c2f1d7fd37
Julie Han <juliemenghan@gmail.com>
2022-07-15 18:23:03 -0400

    can contruct words done

--- can-construct-word.js
+++ can-construct-word.js
@@ -1,3 +1,34 @@
-// add whatever parameters you deem necessary & write doc comment
-function canConstructWord() {
+"use strict";
+
+//input: 2 strings (words and letters)
+//look for characters in word, and count number for times each characters are in word
+//compare count of characters in words vs letters
+//if characters not found in letters or count of characters of words > count in letters
+//then false, else true
+//output: boolean (T or F)
+
+function frequencyCounter(iterable) {
+  let counts = {};
+  for (let item of iterable) {
+    counts[item] ? counts[item]++ : counts[item] = 1;
+  }
+  return counts;
+}
+
+function canConstructWord(words, letters) {
+  if (words.length > letters.length) {
+    return false;
+  }
+  let wordsCount = frequencyCounter(words);
+  let lettersCount = frequencyCounter(letters);
+
+  for (let letter in wordsCount) {
+    if (!lettersCount[letter]) {
+      return false;
+    }
+    if (wordsCount[letter] > lettersCount[letter]) {
+      return false;
+    }
+  }
+  return true;
 }
