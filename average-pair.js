5ef7cc2299387dbb90877dd368a5e96552fd12df
Julie Han <juliemenghan@gmail.com>
2022-07-15 18:56:11 -0400

    two array object

--- average-pair.js
+++ average-pair.js
@@ -9,14 +9,18 @@
 //return boolean TRUE or FLASE
 
 /**
- *
+ * finds IF there is a pair of num in sorted array nums that average of = targetAvg
+ * @param {*} nums
+ * @param {*} targetAvg
+ * @returns true or false
  */
+
 function averagePair(nums, targetAvg) {
   let left = 0;
-  let right = nums.length -1;
+  let right = nums.length - 1;
 
   while (left < right) {
-    const average = (nums[left] + nums[right])/2;
+    const average = (nums[left] + nums[right]) / 2;
 
     if (average === targetAvg) {
       return true;
--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,24 @@
-// add whatever parameters you deem necessary & write docstring
-function twoArrayObject() {
+"use strict";
+
+//input: array of keys + array of values
+//iterate array of keys and add keys to new object
+//add values to keys, null if no values
+//output: object made of arrays
+
+/**
+ * Creates an object from 2 arrays,
+ * if key does not exist for value, ignore value,
+ * if no value for key exists, value = null
+ * @param {*} keys
+ * @param {*} values
+ * @returns object of keys and values
+ */
+function twoArrayObject(keys, values) {
+  let object = new Object();
+
+  for (let i = 0; i < keys.length; i++) {
+    object[keys[i]] = values[i] ? values[i] : null;
+  }
+  return object;
 }
+
