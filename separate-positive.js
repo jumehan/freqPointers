5e64720274930ef87efa8fe66172091c50d954dd
Ryan Rustyn <ryanrustyn@gmail.com>
2022-07-15 17:05:45 -0700

    added for for seperate-positive

--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,27 @@
-// add whatever parameters you deem necessary & write docstring
-function separatePositive() {
+//input an array of non-zero numbers, any length
+//go through array from the left if the number is positive keep it,
+//if the numer is negative push it to the end
+//output: array with positive values grouped left and negative grouped right;
+//  the same array that was entered
+
+//j needed because we're activily modifing array so we need to decrease
+// the amount of array we're looking through
+
+/**
+ * sorts array so positives go to the left and negatives to the right
+ * @param {*} nums
+ * @returns original array
+ */
+
+function separatePositive(nums) {
+  let j = nums.length;
+  for (let i = 0; i < j; i++) {
+    if (nums[i] < 0) {
+      nums.push(nums[i]);
+      nums.splice(i, 1);
+      i--;
+      j--;
+    }
+  }
+  return nums;
 }
